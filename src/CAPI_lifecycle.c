ddb22f4922867cb57e9aa87f54d6ccd7144869f4
lionel550 <123707458+lionel550@users.noreply.github.com>
2026-02-19 21:46:37 -0500

    Reaped all child process at exit

--- src/CAPI_lifecycle.c
+++ src/CAPI_lifecycle.c
@@ -1,9 +1,15 @@
-#include "CAPI_internal.h"
 #include <stdlib.h>
+#include <sys/wait.h>
+#include "CAPI_internal.h"
+
+void CAPI_ReapedChildProcess()
+{
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
 
 void __attribute__((constructor(101))) CAPI_Init()
 {
-    if ((CAPI_RegisterSignalActions() | atexit(CAPI_FreeEndpointRegister)) == -1)
+    if ((CAPI_RegisterSignalActions() | atexit(CAPI_FreeEndpointRegister) | atexit(CAPI_ReapedChildProcess)) == -1)
     {
         exit(EXIT_FAILURE);
     }
