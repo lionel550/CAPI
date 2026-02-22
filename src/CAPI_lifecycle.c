6bec6fb6a57aa18ed534e5b5d2920f8d6fda94d5
lionel550 <123707458+lionel550@users.noreply.github.com>
2026-02-22 15:28:59 -0500

    Improve error handling in signal handler

--- src/CAPI_lifecycle.c
+++ src/CAPI_lifecycle.c
@@ -1,17 +1,30 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include "CAPI_internal.h"
 
-void CAPI_ReapedChildProcess()
+void CAPI_ReapedChildProcesses()
 {
     while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
+CAPI_ErrorCode CAPI_RegisterExitCalls()
+{
+    if ((atexit(CAPI_FreeEndpointRegister) | atexit(CAPI_ReapedChildProcesses)) != 0)
+    {
+        CAPI_SetErrorCode(CAPI_ERROR, "Unable to register exit calls.");
+        return CAPI_ERROR;
+    }
+
+    return CAPI_SUCCESS;
+}
+
 void __attribute__((constructor(101))) CAPI_Init()
 {
-    if ((CAPI_RegisterSignalActions() | atexit(CAPI_FreeEndpointRegister) | atexit(CAPI_ReapedChildProcess)) == -1)
+    if ((CAPI_RegisterSignalActions() | CAPI_RegisterExitCalls()) != CAPI_SUCCESS)
     {
-        exit(EXIT_FAILURE);
+        fprintf(stderr, CAPI_GetLastErrorMessage());
+        exit(CAPI_GetLastErrorCode());
     }
 }
 
--- src/CAPI_signal_handler.c
+++ src/CAPI_signal_handler.c
@@ -1,6 +1,9 @@
+#include <string.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include "CAPI_internal.h"
 
 static volatile sig_atomic_t keep_running = 1;
 
@@ -17,9 +20,15 @@ static void sigint_handler(int s)
 static struct sigaction sa_sigint = {.sa_handler = sigint_handler, .sa_flags = SA_RESTART};
 static struct sigaction sa_sigchild = {.sa_handler = sigchld_handler, .sa_flags = SA_RESTART};
 
-int CAPI_RegisterSignalActions()
+CAPI_ErrorCode CAPI_RegisterSignalActions()
 {
-    return sigaction(SIGCHLD, &sa_sigchild, NULL) | sigaction(SIGINT, &sa_sigint, NULL);
+    if ((sigaction(SIGCHLD, &sa_sigchild, NULL) | sigaction(SIGINT, &sa_sigint, NULL)) == -1)
+    {
+        CAPI_SetErrorCode(CAPI_ERROR, "Unable to register signal actions: %s", strerror(errno));
+        return CAPI_ERROR;
+    }
+
+    return CAPI_SUCCESS;
 }
 
 int CAPI_ShouldKeepRunning()
