121bc288e996aef62db0765e0ad26a2f51b478c2
lionel550 <123707458+lionel550@users.noreply.github.com>
2026-02-25 22:50:53 -0500

    Implement timeout in CAPI_RequestRead to avoid infinite wait

--- src/CAPI_request.c
+++ src/CAPI_request.c
@@ -1,5 +1,7 @@
 #include <errno.h>
+#include <sys/time.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -9,8 +11,10 @@
 
 // TODO: Set a time out
 // TODO: Resolve the 'adress already' use problem
-// TODO: Fix the log proble (CAPI_LOG_INFO("%lu", sizeof(int)))
-CAPI_ErrorCode CAPI_ReadClientRequest(int client_sockfd, char **buffer, size_t *total_bytes_read)
+// TODO: Define it own header file ?
+// TODO: Clean Up error code and rename CAPI_SetErrorCode to CAPI_SetError
+// Create a default config structure in server for: timeout, buffsize, max request size, ....
+CAPI_ErrorCode CAPI_ReadRequest(int client_sockfd, char **buffer, size_t *total_bytes_read)
 {
     char read_buffer[BUFFER_SIZE] = {0};
     char *collect_buffer = malloc(BUFFER_SIZE);
@@ -23,21 +27,60 @@ CAPI_ErrorCode CAPI_ReadClientRequest(int client_sockfd, char **buffer, size_t *
         return CAPI_ERR_MALLOC;
     }
 
-    memset(collect_buffer, '\0', BUFFER_SIZE);
+    memset(collect_buffer, '\0', BUFFER_SIZE); 
+    
+    struct timeval timeout = {0};
+    socklen_t optlen = sizeof(timeout);
 
+    if (getsockopt(client_sockfd, SOL_SOCKET, SO_RCVTIMEO, (void *)&timeout, &optlen) == -1)
+    {
+        CAPI_SetErrorCode(CAPI_ERR_SYS_CALL, "Unable to get the receive timeout: %s", strerror(errno));
+        free(collect_buffer);
+        return CAPI_ERR_SYS_CALL;
+    }
+
+    struct timeval start, now;
+    if (gettimeofday(&start, NULL) == -1)
+    {
+        CAPI_SetErrorCode(CAPI_UNKNOWN_ERR, "Unable to get time of day: %s", strerror(errno));
+        free(collect_buffer);
+        return CAPI_UNKNOWN_ERR;
+    }
+    
     for (;;)
     {
+        if (timeout.tv_usec > 0 || timeout.tv_sec > 0)
+        {
+            if (gettimeofday(&now, NULL) == -1)
+            {
+                CAPI_SetErrorCode(CAPI_UNKNOWN_ERR, "Unable to get time of day: %s", strerror(errno));
+                free(collect_buffer);
+                return CAPI_UNKNOWN_ERR;
+            }
+
+            long elapsed = (now.tv_sec - start.tv_sec) * 1000000 + (now.tv_usec - start.tv_usec);
+
+            if (elapsed >= (timeout.tv_sec * 1000000 + timeout.tv_usec))
+            {
+                CAPI_SetErrorCode(CAPI_UNKNOWN_ERR, "Timeout: the client take too long to send request data.");
+                free(collect_buffer);
+                return CAPI_UNKNOWN_ERR;
+            }
+        }
+
         ssize_t bytes_read = recv(client_sockfd, read_buffer, BUFFER_SIZE - 1, 0);
         
         if (bytes_read == -1)
         {
-            CAPI_SetErrorCode(CAPI_ERR_SYS_CALL, strerror(errno));
+            CAPI_SetErrorCode(CAPI_ERR_SYS_CALL, "Error while receiving request data");
+            free(collect_buffer);
             return CAPI_ERR_SYS_CALL;
         }
         
         if (bytes_read == 0)
         {
             CAPI_SetErrorCode(CAPI_ERR_CLIENT, "Client disconnected.");
+            free(collect_buffer);
             return CAPI_ERR_CLIENT;
         }
    
@@ -48,10 +91,11 @@ CAPI_ErrorCode CAPI_ReadClientRequest(int client_sockfd, char **buffer, size_t *
             if (collect_buffer_capacity == 4 * BUFFER_SIZE)
             {
                 CAPI_SetErrorCode(CAPI_ERR_CLIENT, "The client exceeded the maximum request size.");
+                free(collect_buffer);
                 return CAPI_ERR_CLIENT;
             }
 
-            char * tmp = realloc(collect_buffer, collect_buffer_capacity + BUFFER_SIZE);
+            char *tmp = realloc(collect_buffer, collect_buffer_capacity + BUFFER_SIZE);
             
             if (tmp == NULL)
             {
@@ -75,5 +119,7 @@ CAPI_ErrorCode CAPI_ReadClientRequest(int client_sockfd, char **buffer, size_t *
         }
     }
 
+    CAPI_SetErrorCode(CAPI_UNKNOWN_ERR, "Unexpected error happen where receiving client data.");
+    free(collect_buffer);
     return CAPI_UNKNOWN_ERR;
 }
